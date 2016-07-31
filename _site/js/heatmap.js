08392334b6fff09865b1eba67e0c84dde3e9a360
dfan97 <davidfan219@gmail.com>
2016-07-30 21:23:18 -0400

    Added tooltip hover for the icons, reversed order of jquery and bootstrap.min.js in footer.html

---
+++ _site/js/heatmap.js
@@ -0,0 +1,158 @@
+// var data = [
+//   [20, 20, 80, 20, 234],
+//   [200, 0, 23, 1, 234],
+//   [22, 22, 90, 12, 12],
+//   [250, 255, 253, 231, 234],
+//   [100, 54, 100, 23, 232],
+//   [0, 30, 70, 34, 2],
+//   [255, 13, 8, 34, 2]
+//   ];
+function initHeatmap() {
+    var cluster_method = document.getElementById("cluster_method").value;
+    var distance_metric = document.getElementById("distance_metric").value;
+    
+    var data = dataPro.map(function(d) { return d.map(function(o) { return o.value }); });
+    data.map(function(d, i) { d.name = dataPro[i].name });
+    
+    var clusteredData = clusterfck.hcluster(data, distance_metric, cluster_method);
+    var clusters = convert(clusteredData, "root");
+
+    drawHeatmap(data, clusters);
+}
+
+function drawHeatmap(data, clusters) {
+    
+    var g = document.getElementById('heatmap_panel'),
+	windowWidth = g.clientWidth,
+	windowHeight = g.clientHeight;
+    
+    var nrow = data.length;
+    var ncol = data[0].length;
+    //var minData = d3.min(d3.min(data));
+    //var maxData = d3.max(d3.max(data));
+
+    // remove if already existing for regeneration
+    d3.select("#dendro_svg").remove();
+    
+    // heatmap is width ncol * boxSize
+    // dendrogram is width (ncol * boxSize)*0.5
+    //var width = ncol * boxSizeX + 100;
+    //	height = nrow * boxSizeY;
+    var margin = {top: 0, right: 0, bottom: 0, left: 0},
+	width = windowWidth - margin.left - margin.right,
+	height = windowHeight - margin.top - margin.bottom;
+
+    var boxSizeX = (2*width/3)/ncol; // size of heatmap / ncols
+    var boxSizeY = height/nrow;
+
+    // dendrogram with symmetric children
+    var cluster = d3.layout.cluster()
+	.size([height, (width-boxSizeX)/3]) // size of dendrogram
+	.separation(function(a, b) { return (a.parent == b.parent ? 1 : 1 ) });
+    
+    var diagonal = d3.svg.diagonal()
+	.projection(function(d) { return [d.y, d.x]; });
+
+    var tip = d3.tip()
+	.attr('class', 'd3-tip')
+	.offset([0, 0])
+	.html(function(d) {
+	    return d.children ? 'NA': d.value.name;
+	})    
+
+    var svg = d3.select("#dendro").append("svg")
+	.attr("id","dendro_svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g")
+	.attr("transform", "translate(10,0)");
+
+    svg.call(tip);
+    
+    var nodes = cluster.nodes(clusters),
+	links = cluster.links(nodes);
+    
+    var link = svg.selectAll(".link")
+	.data(links)
+	.enter().append("path")
+	.attr("class", "link")
+	.attr("d", diagonal);
+    
+    var node = svg.selectAll(".node")
+	.data(nodes)
+	.enter().append("g")
+	.attr("class", "node")
+	.attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+
+    // append rectangular boxes if last node (no children)
+    for(var i = 0; i < ncol; i++) {
+
+	// color by row (row normalize color)
+	var minData = d3.min(getCol(data,i));
+	var maxData = d3.max(getCol(data,i));
+	var midData = (minData + maxData) / 2;
+	var colorScale = d3.scale.linear()
+	    .domain([minData, midData, maxData])
+	    .range(["blue","white","red"]);
+
+	node.append("svg:rect")
+	    .attr('width', function(d) { return d.children ? 0 : boxSizeX; })
+	    .attr('height', function(d) { return d.children ? 0 : boxSizeY; })
+	    .attr('x', boxSizeX * i - boxSizeX/2)
+	    .attr('y', -boxSizeY/2)
+	    .attr("stroke", "grey")
+	    .attr("stroke-width", 1)
+	    .style('fill', function(d) { return d.children ? "#fff" : colorScale(d.value[i]) })
+	    .on('mouseover', tip.show)
+	    .on('mouseout', tip.hide);                           
+    }
+
+    /* 
+    node.append("text")
+	.attr("dx", function(d) { return d.children ? 0 : 0; })
+	.attr("dy", 0)
+	.text(function(d) { return d.children ? "" : d.value['name'] });
+    */
+    
+    d3.select(self.frameElement).style("height", height + "px");
+    d3.select(self.frameElement).style("width", width + "px");
+
+
+
+    // Add a legend for the color values.
+    
+    // remove if already existing for regeneration
+    var colorScale = d3.scale.linear()
+        .domain([minData, midData, maxData])
+        .range(["blue","white","red"]);
+
+    var legend = svg.selectAll(".legend")
+        .data(colorScale.ticks(10).reverse())
+	.enter()
+        .append("g")
+        .attr({
+	    'class': 'legend',
+	    'transform': function(d, i) {
+		return "translate(" + 0 + "," + (i * boxSizeY) + ")";
+	    }
+	});
+    
+    legend.append("rect")
+	.attr("stroke", "grey")
+        .attr("stroke-width", 1) 
+	.attr({
+	    'width': boxSizeX,
+	    'height': boxSizeY,
+	    'fill': function(d) {
+		return colorScale(d);
+	    }
+	});
+    
+    svg.append("text")
+        .attr("class", "label")
+        .attr("x", boxSizeX + 5)
+        .attr("y", boxSizeY)
+        .attr("dy", ".35em")
+        .text("Gene Expression");
+    
+}
---
+++ _site/js/pcscatter.js
@@ -0,0 +1,325 @@
+function initDiffexpPca() {
+
+    // sort
+    var sort_method = document.getElementById("diffexp_sort").value;
+    var sort_dir = document.getElementById("diffexp_sortdir").value;
+    var i;
+    if(sort_dir == "decreasing") {
+	i = 1;
+    }
+    if(sort_dir == "increasing") {
+	i = -1;
+    }
+    if(sort_method == "fold-change") {
+	dataPro.map(function(d) { return d.sort(function (a, b) {
+			if (a.fc > b.fc) {
+			    return -i;
+			}
+			if (a.fc < b.fc) {
+			    return i;
+			}
+			// a must be equal to b
+			return 0;
+		    }) });
+    }
+    if(sort_method == "p-value") {
+	dataPro.map(function(d) { return d.sort(function (a, b) {
+			if (a.pval > b.pval) {
+			    return -i;
+			}
+			if (a.pval < b.pval) {
+			    return i;
+			}
+			// a must be equal to b
+			return 0;
+		    }) });
+    }
+
+    drawVolcano(dataPro);
+    drawPval(dataPro);
+    drawFc(dataPro);
+    drawPcScatter(dataPro);
+}
+
+// Draw volcano plot
+function drawVolcano(dataPro) {
+
+    var data = dataPro[0].map(function(o) { return { name: o.name, fc: o.fc, pval: o.pval } });
+
+    var g = document.getElementById('volcano_panel'),
+	windowWidth = g.clientWidth,
+	windowHeight = g.clientHeight;
+
+    var margin = {top: 15, right: 10, bottom: 50, left: 30},
+        width = windowWidth - margin.left - margin.right,
+        height = windowHeight - margin.top - margin.bottom;
+
+    // remove if already existing for regeneration
+    d3.select("#volcano_svg").remove();
+
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-1, 0])
+        .html(function(d) {
+	    return d.name;
+	})
+
+    var svg = d3.select("#volcano").append("svg")
+	.attr("id","volcano_svg")
+	.attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.call(tip);
+
+    x.domain(d3.extent(data, function(d) { return d.fc; })).nice();
+    y.domain(d3.extent(data, function(d) { return d.pval; })).nice();
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("log2(fold change)");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("-log10(p-value)")
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 5)
+        .attr("cx", function(d) { return x(d.fc); })
+        .attr("cy", function(d) { return y(d.pval); })
+	.style("fill", function(d) {
+	    if (d.pval >= -Math.log10(0.05/data.length)) {return "red"}
+	    else { return "steelblue" }
+	    ;})
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
+    svg.append("line")
+	.attr("x1", 0)
+	.attr("y1", y(-Math.log10(0.05/data.length)))
+	.attr("x2", width)
+	.attr("y2", y(-Math.log10(0.05/data.length)))
+	.style("stroke-width", 1)
+	.style("stroke-dasharray", "5,5")
+	.style("stroke", "red")
+	.style("fill", "none");
+}
+
+// Barplot of p-values
+function drawPval(dataPro) {
+
+    var data = dataPro[0].map(function(o) { return { name: o.name, value: o.pval } });
+    var threshold = -Math.log10(0.05/data.length);
+    
+    var g = document.getElementById('diffexp_pval_panel'),
+	windowWidth = g.clientWidth,
+	windowHeight = g.clientHeight
+
+    var margin = {top: 15, right: 0, bottom: 15, left: 30},
+        width = windowWidth - margin.left - margin.right,
+        height = windowHeight - margin.top - margin.bottom;
+
+    // remove if already existing for regeneration
+    d3.select("#diffexp_pval_svg").remove();
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1)
+        .domain(data.map(function(d) { return d.name; }));
+
+    var y = d3.scale.linear()
+        .range([height, 0])
+        .domain([0, d3.max(data.map(function(d) { return d.value }))]);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(5);
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-1, 0])
+        .html(function(d) {
+	    return d.name;
+	})
+
+    var svg = d3.select("#diffexp_pval_panel").append("svg")
+        .attr("id","diffexp_pval_svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.call(tip);
+
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.name); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); })
+        .style("fill", function(d) {
+	    if (d.value >= threshold) {return "red"}
+	    else { return "steelblue" }
+	})
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+
+    svg.append("line")
+        .attr("y1", y(threshold))
+        .attr("x1", 0)
+        .attr("y2", y(threshold))
+        .attr("x2", width)
+        .style("stroke-width", 1)
+        .style("stroke-dasharray", "5,5")
+        .style("stroke", "red")
+        .style("fill", "none");
+
+}
+
+// Barplot of fold-change
+function drawFc(dataPro) {
+
+    var data = dataPro[0].map(function(o) { return { name: o.name, value: o.fc } });
+
+    // remove if already existing for regeneration
+    d3.select("#diffexp_fc_svg").remove();
+
+    var g = document.getElementById('diffexp_fc_panel'),
+	windowWidth = g.clientWidth,
+	windowHeight = g.clientHeight,
+        margin = {top: 15, right: 0, bottom: 15, left: 30}
+
+    var svg = d3.select("#diffexp_fc").datum(data)
+        .attr("id","diffexp_fc_svg")
+	.call(columnChart()
+	      .margin(margin)
+	      .width(windowWidth)
+	      .height(windowHeight)
+	      .x(function(d, i) { return d.name; })
+	      .y(function(d, i) { return d.value; }))
+
+}
+
+// PCA
+function drawPcScatter(dataPro) {
+
+    var data = dataPro;
+
+    var g = document.getElementById('pca_panel'),
+	windowWidth = g.clientWidth,
+	windowHeight = g.clientHeight;
+
+    var margin = {top: 15, right: 15, bottom: 30, left: 30},
+        width = windowWidth - margin.left - margin.right,
+        height = windowHeight - margin.top - margin.bottom;
+
+    // remove if already existing for regeneration
+    d3.select("#pca_svg").remove();
+
+    var x = d3.scale.linear()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var color = d3.scale.category10();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-1, 0])
+        .html(function(d) {
+	    return d.name;
+	})
+
+    var svg = d3.select("#pca").append("svg")
+	.attr("id","pca_svg")
+	.attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.call(tip);
+
+    x.domain(d3.extent(data, function(d) { return d.pc1; })).nice();
+    y.domain(d3.extent(data, function(d) { return d.pc2; })).nice();
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("PC1");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("PC2")
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 5)
+        .attr("cx", function(d) { return x(d.pc1); })
+        .attr("cy", function(d) { return y(d.pc2); })
+        .style("fill", function(d) { return color(d.group); })
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+}
