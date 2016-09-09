da4c24e14cf837a8a3187a6704a1b0305c314125
JEFworks <jeanfan@fas.harvard.edu>
2016-09-09 15:04:04 -0400

    Merge David's branch

--- _site/README.md
+++
@@ -1,5 +0,0 @@
-UBiT2 is a free, open-source, user-friendly web-service that enables fast, simple bioinformatics analysis online. No downloads, installations, registration, or accounts are needed. Your data is never accessed or stored in any way. Using just a browser, users can copy and paste in their data to perform standard analyses such as quality control, filtering, hierarchical clustering, principal component analysis, differential expression analysis, gene set enrichment testing, and more, all with interactive visualizations.
-
-# Try it out ☞ [ubit2.com](http://ubit2.com/)  
-
-<img src="screenshot-ubit2.com.png" width="100%">
--- js/pcscatter.js
+++ js/pcscatter.js
@@ -322,4 +322,68 @@ function drawPcScatter(dataPro) {
         .style("fill", function(d) { return color(d.group); })
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
+
+    legend = svg.append("g")
+        .attr("class","legend")
+        .attr("transform","translate(" + width + ", 0)")
+        .style("font-size","12px")
+        .call(d3.legend)
+    setTimeout(function() {
+	legend
+	    .style("font-size","20px")
+	    .attr("data-style-padding",10)
+	    .call(d3.legend)
+    },1000)
 }
+
+(function() {
+    d3.legend = function(g) {
+	g.each(function() {
+	    var g= d3.select(this),
+		items = {},
+		svg = d3.select(g.property("nearestViewportElement")),
+		legendPadding = g.attr("data-style-padding") || 5,
+		lb = g.selectAll(".legend-box").data([true]),
+		li = g.selectAll(".legend-items").data([true])
+
+	    lb.enter().append("rect").classed("legend-box",true)
+	    li.enter().append("g").classed("legend-items",true)
+
+	    svg.selectAll("[data-legend]").each(function() {
+		var self = d3.select(this)
+		items[self.attr("data-legend")] = {
+		    pos : self.attr("data-legend-pos") || this.getBBox().y,
+		    color : self.attr("data-legend-color") != undefined ? self.attr("data-legend-color") : self.style("fill") != 'none' ? self.style("fill") : self.style("stroke")
+		}
+	    })
+
+		items = d3.entries(items).sort(function(a,b) { return a.value.pos-b.value.pos})
+
+
+	    li.selectAll("text")
+	        .data(items,function(d) { return d.key})
+	        .call(function(d) { d.enter().append("text")})
+	        .call(function(d) { d.exit().remove()})
+	        .attr("y",function(d,i) { return i+"em"})
+	        .attr("x","1em")
+	        .text(function(d) { ;return d.key})
+
+	    li.selectAll("circle")
+	        .data(items,function(d) { return d.key})
+	        .call(function(d) { d.enter().append("circle")})
+	        .call(function(d) { d.exit().remove()})
+	        .attr("cy",function(d,i) { return i-0.25+"em"})
+	        .attr("cx",0)
+	        .attr("r","0.4em")
+	        .style("fill",function(d) { console.log(d.value.color);return d.value.color})
+
+	    // Reposition and resize the box
+	    var lbbox = li[0][0].getBBox()
+	    lb.attr("x",(lbbox.x-legendPadding))
+	        .attr("y",(lbbox.y-legendPadding))
+	        .attr("height",(lbbox.height+2*legendPadding))
+	        .attr("width",(lbbox.width+2*legendPadding))
+	})
+	    return g
+    }
+})()
